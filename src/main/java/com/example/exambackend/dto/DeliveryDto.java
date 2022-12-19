9fd45495e2cdb00984d05a19dbb76bc81e0463f8
asgerbirk <asger.birk@gmail.com>
2022-12-19 11:09:28 +0100

    opgave 2 done

---
+++ src/main/java/com/example/exambackend/dto/DeliveryDto.java
@@ -0,0 +1,4 @@
+package com.example.exambackend.dto;
+
+public class DeliveryDto {
+}
---
+++ src/main/java/com/example/exambackend/service/DeliveryService.java
@@ -0,0 +1,10 @@
+package com.example.exambackend.service;
+
+import com.example.exambackend.dto.ProductMapper;
+import com.example.exambackend.repository.DeliveryRepository;
+import com.example.exambackend.repository.ProductRepository;
+
+public class DeliveryService {
+
+    private final DeliveryRepository deliveryRepository;
+}
